079f6726f44756ad2ddc066b5d4f44220e4701fa
rtgotthepower33 <ryan-tatum@Pluralsight.com>
2024-06-13 07:54:22 -0600

    button move

---
+++ script.js
@@ -0,0 +1,11 @@
+const initSlider = () => {
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            console.log(button);
+        });
+    });
+}
+
+window.addEventListener("load", initSlider);
