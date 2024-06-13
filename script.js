f082b02825035c9552375ca784371d5e29261da8
rtgotthepower33 <ryan-tatum@Pluralsight.com>
2024-06-13 08:40:30 -0600

    new

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const initSlider = () => {
     const imageList = document.querySelector(".slider-wrapper .slide-button");
     const slideButtons = document.querySelectorAll(".slider-wrapper .image-list");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
@@ -9,6 +10,14 @@ const initSlider = () => {
             imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
         });
     });
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    imageList.addEventListener("scroll", () =>{
+        handleSlideButtons()
+    });    
 }
 
 window.addEventListener("load", initSlider);
