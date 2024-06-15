33403442ff56ca73f09d6f00426434c90ec62997
rtgotthepower33 <ryan-tatum@Pluralsight.com>
2024-06-15 10:47:50 -0600

    progrss

--- script.js
+++ script.js
@@ -56,3 +56,16 @@ const initSlider = () => {
 }
 
 window.addEventListener("load", initSlider);
+
+const bgAnimation = document.getElementById
+('bgAnimation');
+
+const numberOfColorBoxes = 400;
+
+for(let i = 0; i < numberOfColorBoxes; i++){
+    const colorBox = document.createElement
+    ('div');
+
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
